3c3be7027a3f8fb00ea66c5b4e64101a0f5278d3
Titus Kemboi Cheserem <cheseremtitus24@gmail.com>
2023-01-23 14:45:02 +0300

    new file:   2-selection_sort.c 	modified:   sort.h 	modified:   0-main.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "sort.h"
+/**
+ * selection_sort-Sorts an array using Selection Sorting
+ * @array: Input array to be sorted
+ * @size: Size of the array
+ * Description:
+ * selection involves scanning through the
+ * entire list and choosing the smallest
+ *
+ * Return: void
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, position, swap;
+
+	for (i = 0; i < (size - 1); i++)
+	{
+		position = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[position] > array[j])
+				position = j;
+		}
+		if (position != i)
+		{
+			swap = array[i];
+			array[i] = array[position];
+			array[position] = swap;
+		}
+		print_array(array, size);
+	}
+}
